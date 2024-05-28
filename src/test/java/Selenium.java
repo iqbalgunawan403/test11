7f618a90d15401c6062d7b38bc423ce487f72116
DaffaPintu <daffa@pintu.co.id>
2024-05-28 09:46:28 +0700

    FIX

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,14 +1,35 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
 
 
 public class Selenium {
-    @Test
-    public void helloIqbal(){
-        WebDriver driver = WebDriverManager.chromedriver().create();
-        driver.get("https://www.google.com/");
 
+    public static WebDriver driver;
+    @Test
+    public void helloIqbal() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://jayjay.co/");
+        Thread.sleep(1000);
+        driver.navigate().to("https://selenium.dev");
+        Thread.sleep(1000);
+        driver.navigate().back();
+        Thread.sleep(1000);
+        driver.quit();
+        System.out.println("Test Selenium Success");
     }
 }

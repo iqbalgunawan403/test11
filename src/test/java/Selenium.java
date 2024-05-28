70b2b5ba56e7e936fb8fd263eabc0b3ad5e95e9a
Iqbal Gunawan <iqbalgunawan403@gmail.com>
2024-05-28 08:42:52 +0700

    Added my project

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Selenium {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+
+
+public class Selenium {
+    @Test
+    public void helloIqbal(){
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.get("https://www.google.com/");
+
+    }
 }
